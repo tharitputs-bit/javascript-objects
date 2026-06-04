fd337ccd032f6d0edc6bd05eee075b2bc312ce85
KT <felikzz98@gmail.com>
2026-06-04 14:04:25 +0700

    feat: implement object property manipulations and refactor console logs for exercises 1-3

--- ex-3.js
+++ ex-3.js
@@ -13,17 +13,6 @@ const employee = {
   },
 };
 
-console.log("Employee Name: " + employees.name);
-console.log("Employee Age: " + employee.age);
-console.log(
-  "Employee Address: " +
-    employee.address.street +
-    ", " +
-    employee.address.city +
-    ", " +
-    employee.address.state +
-    ", " +
-    employee.address.country +
-    " " +
-    employee.adress.postalCode
-);
+console.log(`Employee Name: ${employee.name}`);
+console.log(`Employee Age: ${employee.age}`);
+console.log(`Employee Address: ${employee.address.street}, ${employee.address.city}, ${employee.address.states}, ${employee.address.country}, ${employee.address.postalCode}`);
